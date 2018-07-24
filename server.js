da2b3cf26a3cabbae6623d6036333f1a2c78f476
kgodwin88 <kgodwin1988@aol.com>
2018-07-23 21:03:35 -0400

    updated port on server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
